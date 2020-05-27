3d532c7f0921a3c3f1486afc52f60dade4e93db9
Zong-Z <nazarslota20036@gmail.com>
2020-05-27 20:52:36 +0300

    Added the necessary structures and the ability to work with commands.

---
+++ betypes/text.go
@@ -0,0 +1,18 @@
+package betypes
+
+//Start command data
+const (
+	StartCommand = "start"
+	StartText    = "START_TEXT"
+)
+
+//Help command data
+const (
+	HelpCommand = "help"
+	HelpText    = "HELP_TEXT"
+)
+
+//Unclear command data
+const (
+	UnclearCommandText = "Unclear command"
+)
