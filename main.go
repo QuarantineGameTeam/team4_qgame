e29302971aa045793222757a77b6e4cccf04a9f8
M-Bell <maxymshyjko@gmail.com>
2020-06-01 21:15:21 +0300

    team4_qgame

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
 	"team4_qgame/betypes"
+	"team4_qgame/db"
 	"team4_qgame/loger"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -13,28 +17,43 @@ var (
 	NewBot, BotErr = tgbotapi.NewBotAPI(betypes.BOT_TOKEN)
 )
 
+func getUser(msg *tgbotapi.Message) betypes.User {
+	user := betypes.User{Id: msg.Chat.ID, FirstName: msg.Chat.FirstName, Username: msg.Chat.UserName, Rank: 0}
+	fmt.Println(user)
+	return user
+}
+
+func checkOnCommands(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+	var user betypes.User
+	if update.Message.Text != "/start" {
+		user = db.GetUser(strconv.Itoa(int(update.Message.Chat.ID)))
+	}
+	if update.Message.IsCommand() {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case betypes.StartCommand:
+			user = getUser(update.Message)
+			db.SaveUser(user)
+		case betypes.HelpCommand:
+			msg.Text = betypes.HelpText
+
+		default:
+			msg.Text = betypes.UnclearCommandText
+		}
+		bot.Send(msg)
+
+	}
+	db.SaveUser(user)
+}
+
 func main() {
+
 	go func() {
 		log.Fatal(http.ListenAndServe(":"+betypes.BOT_PORT, nil))
 	}()
 	loger.ForError(BotErr, "BOT_TOKEN error")
 
 	getUpdates(NewBot)
-}
-
-func checkOnCommands(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	if update.Message.IsCommand() {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		switch update.Message.Command() {
-		case betypes.StartCommand:
-			msg.Text = betypes.StartText
-		case betypes.HelpCommand:
-			msg.Text = betypes.HelpText
-		default:
-			msg.Text = betypes.UnclearCommandText
-		}
-		bot.Send(msg)
-	}
 }
 
 func setWebhook(bot *tgbotapi.BotAPI) {
@@ -45,6 +64,7 @@ func setWebhook(bot *tgbotapi.BotAPI) {
 func getUpdates(bot *tgbotapi.BotAPI) {
 	setWebhook(bot)
 	updates := bot.ListenForWebhook("/")
+
 	for update := range updates {
 		checkOnCommands(update, bot)
 	}
