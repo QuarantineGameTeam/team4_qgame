6a74105be3eec0b175bc7346b758d369a1f34ad3
Zong-Z <nazarslota20036@gmail.com>
2020-06-02 14:44:48 +0300

    Added start command.

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"team4_qgame/actions"
 
 	"team4_qgame/betypes"
-	"team4_qgame/db"
 	"team4_qgame/loger"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -17,37 +15,22 @@ var (
 	NewBot, BotErr = tgbotapi.NewBotAPI(betypes.BOT_TOKEN)
 )
 
-func getUser(msg *tgbotapi.Message) betypes.User {
-	user := betypes.User{Id: msg.Chat.ID, FirstName: msg.Chat.FirstName, Username: msg.Chat.UserName, Rank: 0}
-	fmt.Println(user)
-	return user
-}
-
 func checkOnCommands(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	var user betypes.User
-	if update.Message.Text != "/start" {
-		user = db.GetUser(strconv.Itoa(int(update.Message.Chat.ID)))
-	}
 	if update.Message.IsCommand() {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		switch update.Message.Command() {
 		case betypes.StartCommand:
-			user = getUser(update.Message)
-			db.SaveUser(user)
+			actions.StartCommand(&update, bot)
 		case betypes.HelpCommand:
 			msg.Text = betypes.HelpText
-
 		default:
 			msg.Text = betypes.UnclearCommandText
 		}
 		bot.Send(msg)
-
 	}
-	db.SaveUser(user)
 }
 
 func main() {
-
 	go func() {
 		log.Fatal(http.ListenAndServe(":"+betypes.BOT_PORT, nil))
 	}()
