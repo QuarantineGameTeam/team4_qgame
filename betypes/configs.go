c94d6686171d8276166bcc368dbb55c8ac068a94
rh-pr <65288995+rh-pr@users.noreply.github.com>
2020-06-03 22:38:43 +0300

    Update configs.go

--- betypes/configs.go
+++ betypes/configs.go
@@ -2,10 +2,10 @@ package betypes
 
 //Bot settings
 const (
-	BOT_TOKEN = "1103065557:AAGUh9bfw0DrsMC0UGuz1zmi-TP3mtPxuXs" //Your bot token
+	BOT_TOKEN = "" //Your bot token
 	//TELEGRAM_URL = "https://api.telegram.org/bot"
 	BOT_PORT = "8090"
-	WEB_HOOK = "https://af94b7b81058.ngrok.io" //Ngrok url
+	WEB_HOOK = "" //Ngrok url
 )
 
 
